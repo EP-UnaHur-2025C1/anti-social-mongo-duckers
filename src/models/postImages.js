c70f0f868cafd288acbf3ef01ea64e4745796cab
alymarquez <alyym.55@gmail.com>
2025-06-12 17:38:50 -0300

    Modelo de postImages

---
+++ src/models/postImages.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+// const validator = require("validator");
+
+const postImagesSchema = new mongoose.Schema({
+  url: {
+    type: String,
+    required: [true,"La url es obligatoria"]
+    //validate: [validator.isUrl, "Url invalida"]
+  },
+  post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post"
+    }
+});
+
+module.exports = mongoose.model("PostImages", postImagesSchema);
